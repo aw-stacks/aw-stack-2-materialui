f2b618494abeeb5f3d3dadf2bb5806f41f9bda6e
Ashwinh <ashwinh@cybage.com>
2016-11-22 19:47:08 +0530

    Add initial stack

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
+import { MaterialModule } from "@angular/material";
+
+import { rootRouterConfig } from "./router/app.routes";
+import { LayoutComponent } from "./pages/layout/layout.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+
+import "./styles/common.less";
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig),
+    MaterialModule.forRoot()
+  ],
+  bootstrap: [LayoutComponent],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }]
+})
+
+export class AppModule { }
